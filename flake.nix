3560ef1cac02173b7e162b034a56bedf49321cb2
tappunk <me@tappunk.com>
2026-06-17 13:25:10 +0900

    build(nix): modernize flake with flake-utils, dynamic version, devShell, and apps
    - Add flake-utils input for cleaner system iteration
    - Read version dynamically from Cargo.toml via builtins.fromTOML
    - Move nativeBuildInputs and buildInputs into package definition
    - Add devShell.default with Rust toolchain and framework dependencies
    - Add apps.default for nix app support
    - Hardcode platform list to aarch64-darwin

--- flake.nix
+++ flake.nix
@@ -3,46 +3,80 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
-        pname = "muthr";
-        version = "0.1.7";
-        src = ./.;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "aarch64-darwin" ] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
 
-        cargoLock = {
-          lockFile = ./Cargo.lock;
+        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+        version = cargoToml.package.version;
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "muthr";
+          inherit version;
+          src = ./.;
+
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+
+          nativeBuildInputs = with pkgs; [
+            installShellFiles
+            pkg-config
+          ];
+
+          buildInputs = with pkgs.darwin.apple_sdk.frameworks; [
+            Security
+            SystemConfiguration
+            Metal
+            Foundation
+            Accelerate
+          ];
+
+          postInstall = ''
+            installShellCompletion --cmd muthr \
+              --bash <($out/bin/muthr completion bash) \
+              --zsh <($out/bin/muthr completion zsh)
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Zero-trust orchestration for autonomous AI agents";
+            homepage = "https://github.com/tappunk/muthr";
+            license = licenses.mit;
+            maintainers = [ ];
+            platforms = [ "aarch64-darwin" ];
+            mainProgram = "muthr";
+          };
         };
 
-        nativeBuildInputs = [ pkgs.installShellFiles ];
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustc
+            cargo
+            clippy
+            rustfmt
+            rust-analyzer
 
-        # Force usage of modern SDK frameworks provided by the system
-        buildInputs = with pkgs.darwin.apple_sdk.frameworks; [
-          Security
-          SystemConfiguration
-          Metal
-          Foundation
-          Accelerate
-        ];
+            darwin.apple_sdk.frameworks.Security
+            darwin.apple_sdk.frameworks.SystemConfiguration
+            darwin.apple_sdk.frameworks.Metal
+            darwin.apple_sdk.frameworks.Foundation
+            darwin.apple_sdk.frameworks.Accelerate
+          ];
 
-        postInstall = ''
-          installShellCompletion --cmd muthr \
-            --bash <($out/bin/muthr completion bash) \
-            --zsh <($out/bin/muthr completion zsh)
-        '';
-
-        meta = with pkgs.lib; {
-          description = "Zero-trust orchestration for autonomous AI agents";
-          homepage = "https://github.com/tappunk/muthr";
-          license = licenses.mit;
-          platforms = [ "aarch64-darwin" ];
+          shellHook = ''
+            echo "muthr dev environment loaded (aarch64-darwin)"
+            echo "Version: ${version}"
+          '';
         };
-      };
-    };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/muthr";
+        };
+      });
 }
